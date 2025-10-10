2fb4c3da08c30252bfec7984edb4c86854f3b992
lj-sec <126737129+lj-sec@users.noreply.github.com>
2025-10-10 17:35:48 -0400

    Security.nix for omen30l

--- modules/core/services/security.nix
+++ modules/core/services/security.nix
@@ -3,19 +3,19 @@
   services.dbus.enable = true;
   security.polkit.enable = true;
   services.fwupd.enable = true;
-  services.fprintd.enable = true;
+  # services.fprintd.enable = true;
 
-  security.pam.services = {
-    login.fprintAuth = true;
-    sudo.fprintAuth = true;
-    "polkit-1".fprintAuth = true;
-    hyprlock.fprintAuth = true;
-  };
+  # security.pam.services = {
+  #   login.fprintAuth = true;
+  #   sudo.fprintAuth = true;
+  #   "polkit-1".fprintAuth = true;
+  #    hyprlock.fprintAuth = true;
+  # };
   
 
-  environment.systemPackages = with pkgs; [
-    fprintd
-    libfprint
-    pamtester
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   fprintd
+  #   libfprint
+  #   pamtester
+  # ];
 }
